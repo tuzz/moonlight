13628b20e4f7012daa07819f6137d1311abc6723
Chris Patuzzo <chris@patuzzo.co.uk>
2019-02-26 14:26:29 +0000

    Start to use the Intersection’s data for ray tracing
    Currently, we have no lights, so color the sphere
    based on the surface normal at the intersection.

--- src/systems/ray_tracer/mod.rs
+++ src/systems/ray_tracer/mod.rs
@@ -25,12 +25,24 @@ impl<'a> System<'a> for RayTracer {
         for (_, transform, fov, image) in (&cameras, &transforms, &fovs, &mut images).join() {
             let ray_generator = RayGenerator::new(transform, fov, image);
 
+            // For each ray that goes through a pixel in the image plane:
             for (coordinate, ray) in ray_generator.generate() {
-                for (_, transform, material) in (&spheres, &transforms, &materials).join() {
-                    if let Some(_) = IntersectionChecker::<Sphere>::intersection(&ray, transform) {
-                        image.set(&coordinate, material.color);
-                    }
+
+                // Get the closest intersection/material (if any) for the ray:
+                let closest = (&spheres, &transforms, &materials).join()
+                    .map(|(_, t, m)| (IntersectionChecker::<Sphere>::intersection(&ray, t), m))
+                    .filter_map(|(option, m)| option.map(|i| (i, m)))
+                    .min_by(|(a, _), (b, _)| a.cmp(&b));
+
+                // Continue if the ray doesn't intersect:
+                if closest.is_none() {
+                    continue;
                 }
+
+                let (intersection, material) = closest.unwrap();
+
+                // Temporarily set the color based on the surface normal:
+                image.set(&coordinate, intersection.normal);
             }
         }
     }
