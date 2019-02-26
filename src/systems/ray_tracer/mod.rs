95941eb2ffd0c47f1321aad68eede6f4212700a0
Chris Patuzzo <chris@patuzzo.co.uk>
2019-02-26 13:23:26 +0000

    Set the pixel color to red for ray-sphere intersections

--- src/systems/ray_tracer/mod.rs
+++ src/systems/ray_tracer/mod.rs
@@ -4,6 +4,7 @@ mod intersection;
 mod intersection_checker;
 
 use specs::prelude::*;
+use cgmath::Vector3;
 use crate::components::*;
 use ray_generator::RayGenerator;
 use intersection_checker::IntersectionChecker;
@@ -16,14 +17,20 @@ impl<'a> System<'a> for RayTracer {
         ReadStorage<'a, Transform>,
         ReadStorage<'a, FieldOfView>,
         WriteStorage<'a, Image>,
+
+        ReadStorage<'a, Sphere>,
     );
 
-    fn run(&mut self, (camera, transform, fov, mut image): Self::SystemData) {
-        for (_, transform, fov, image) in (&camera, &transform, &fov, &mut image).join() {
+    fn run(&mut self, (cameras, transforms, fovs, mut images, spheres): Self::SystemData) {
+        for (_, transform, fov, image) in (&cameras, &transforms, &fovs, &mut images).join() {
             let ray_generator = RayGenerator::new(transform, fov, image);
 
             for (coordinate, ray) in ray_generator.generate() {
-                image.set(&coordinate, ray.direction);
+                for (_, transform) in (&spheres, &transforms).join() {
+                    if let Some(_) = IntersectionChecker::<Sphere>::intersection(&ray, transform) {
+                        image.set(&coordinate, Vector3::new(1.0, 0.0, 0.0));
+                    }
+                }
             }
         }
     }
--- src/systems/scene_generator/mod.rs
+++ src/systems/scene_generator/mod.rs
@@ -1,5 +1,5 @@
 use specs::prelude::*;
-use cgmath::{Vector2, Matrix4, SquareMatrix};
+use cgmath::{Vector2, Vector3, Matrix4, SquareMatrix};
 use crate::components::{Camera, Transform, FieldOfView, Image, Name, Sphere};
 
 pub struct SceneGenerator;
@@ -12,14 +12,14 @@ impl<'a> System<'a> for SceneGenerator {
 
         lazy.insert(camera, Camera::new());
         lazy.insert(camera, Transform::new(Matrix4::identity()));
-        lazy.insert(camera, FieldOfView::new(Vector2::new(90.0, 90.0)));
+        lazy.insert(camera, FieldOfView::new(Vector2::new(86.0, 55.36)));
         lazy.insert(camera, Image::new(Vector2::new(1920, 1080)));
         lazy.insert(camera, Name::new("camera-1"));
 
         let sphere = entities.create();
 
         lazy.insert(sphere, Sphere::new());
-        lazy.insert(sphere, Transform::new(Matrix4::identity()));
+        lazy.insert(sphere, Transform::new(Matrix4::from_translation(Vector3::new(0.0, 0.0, 5.0))));
         lazy.insert(sphere, Name::new("sphere-1"));
     }
 }
